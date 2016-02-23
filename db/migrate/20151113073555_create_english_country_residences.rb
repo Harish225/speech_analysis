32cd41119fc070c4c71f42597be5067a5d6fde57
Harish225 <harish.dhara@gmail.com>
2016-02-22 18:13:07 -0700

    Duplicate File

--- db/migrate/20151113073555_create_english_country_residences.rb
+++
@@ -1,9 +0,0 @@
-class CreateEnglishCountryResidences < ActiveRecord::Migration
-  def change
-    create_table :english_country_residences do |t|
-      t.string :name
-
-      t.timestamps null: false
-    end
-  end
-end
